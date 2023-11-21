3b6b85ab8b6f484a57f44e785b18ee9619b8f68d
agash1809 <agaash1809@gmail.com>
2023-11-21 15:42:55 +0530

    first review

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import Home from './Home.js';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import SignIn from './SignIn';
+// import BankingSignUp from './Signup';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Routes>
+//           <Route path='/' element={<Home/>}/>
+//           <Route path='login' element={<SignIn/>}/>
+//           <Route path='signup' element={<BankingSignUp/>}/>
+//         </Routes>
+//       </Router>
+
+    
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import Home from './Home.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './SignIn';
+import BankingSignUp from './Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='login' element={<SignIn />} />
+          <Route path='signup' element={<BankingSignUp />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
